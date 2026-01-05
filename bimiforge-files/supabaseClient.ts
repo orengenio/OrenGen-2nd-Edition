d31f2b5581d2f5b9baceb2311c560b2e7f1d3bc6
OrenGen Worldwide <sales@orengen.io>
2026-01-05 03:43:33 -0600

    Delete bimiforge-files directory

--- bimiforge-files/supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL = 'https://bimiforge.orengen.io';
-const SUPABASE_ANON_KEY = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdXBhYmFzZSIsImlhdCI6MTc2NzEzMjk2MCwiZXhwIjo0OTIyODA2NTYwLCJyb2xlIjoiYW5vbiJ9.a5BAKuz7bunKbLmSwCDgj8a9HyNIfcWHYegsU7spCJc';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
